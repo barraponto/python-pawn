417efe0a9d163f41f7f3aab7657511fd2c105851
Capi Etheriel <barraponto@gmail.com>
2015-03-22 03:17:22 -0300

    [pawn] started development.

---
+++ src/pawn/game.py
@@ -0,0 +1,13 @@
+from pawn.player import Player
+
+class Game():
+    player_class = Player
+    supported_number_of_players = [2, 4]
+
+    def __init__(self, number_of_players=2):
+        if number_of_players not in self.supported_number_of_players:
+            raise ValueError(
+                'Unsupported number of players: {}.'.format(number_of_players))
+        else:
+            self.players = [self.player_class(name='Player {}'.format(n))
+                            for n in range(1, 1 + number_of_players)]
--- tests/test_pawn.py
+++ tests/test_pawn.py
@@ -1,5 +1,18 @@
-from pawn.__main__ import main
+import pytest
 
+from pawn.game import Game
+from pawn.game import Player
+
+
+@pytest.mark.parametrize('number_of_players',
+                         [2, pytest.mark.xfail(3), 4])
+def test_number_of_players(number_of_players):
+    assert len(Game(number_of_players=number_of_players).players) == number_of_players
+
+@pytest.mark.parametrize('name', ['Fox', 'Falco', 'Peppy', 'Slippy'])
+def test_player_name(name):
+    assert Player(name).name == name
 
 def test_main():
+    from pawn.__main__ import main
     assert main([]) == 0
