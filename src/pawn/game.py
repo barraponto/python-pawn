6b38602e0b403a262dfb694aa91e6862114b30b5
Capi Etheriel <barraponto@gmail.com>
2015-03-23 23:16:48 -0300

    [game] add game `__repr__`

--- src/pawn/game.py
+++ src/pawn/game.py
@@ -1,5 +1,6 @@
 from pawn.player import Player
 
+
 class Game(object):
     player_class = Player
     supported_number_of_players = [2, 4]
@@ -12,3 +13,7 @@ class Game(object):
         else:
             self.players = [self.player_class(name='Player {number}'.format(
                 number=n)) for n in range(1, 1 + number_of_players)]
+
+    def __repr__(self):
+        return 'Game(number_of_players={number!r})'.format(
+            number=self.number_of_players)
