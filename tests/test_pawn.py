74c757008e5e0d09b31f00f295d31652e984cff8
Capi Etheriel <barraponto@gmail.com>
2015-03-24 00:26:13 -0300

    [tests] covers `__repr__` methods

--- tests/test_pawn.py
+++ tests/test_pawn.py
@@ -7,11 +7,16 @@ from pawn.game import Player
 @pytest.mark.parametrize('number_of_players',
                          [2, pytest.mark.xfail(3), 4])
 def test_number_of_players(number_of_players):
-    assert len(Game(number_of_players=number_of_players).players) == number_of_players
+    game = Game(number_of_players=number_of_players)
+    assert len(game.players) == number_of_players
+    assert repr(game) == "Game(number_of_players={number})".format(
+        number=number_of_players)
 
 @pytest.mark.parametrize('name', ['Fox', 'Falco', 'Peppy', 'Slippy'])
 def test_player_name(name):
-    assert Player(name).name == name
+    player = Player(name=name)
+    assert player.name == name
+    assert repr(player) == "Player(name={name!r})".format(name=name)
 
 def test_main():
     from pawn.__main__ import main
